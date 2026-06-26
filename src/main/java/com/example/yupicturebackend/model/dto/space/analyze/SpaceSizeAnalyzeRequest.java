5b52bfc4b0c8f8937f4dccffbeaa29e08e8e9183
skyvoice10 <107035777+skyvoice10@users.noreply.github.com>
2026-06-26 17:41:29 +0800

    Add files via upload

---
+++ src/main/java/com/example/yupicturebackend/model/dto/space/analyze/SpaceSizeAnalyzeRequest.java
@@ -0,0 +1,12 @@
+package com.example.yupicturebackend.model.dto.space.analyze;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 空间图片大小分析请求
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SpaceSizeAnalyzeRequest extends SpaceAnalyzeRequest{
+}
